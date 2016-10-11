299d571389290da6b97b3c7430d0e533d1c17113
jcheung29 <jcheung@albany.edu>
2016-10-11 18:01:52 -0400

    1st edit
    started the creation of the class node

---
+++ linkedList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class linkedList {
+
+class node {
+  String value; //element 
+  Node next; // reference to successor
+  Node prev; // reference to predecessor 
+  
+  Node (String val, Node n, Node p) {
+  value = val;
+  next = n;
+  prev = p;
+  }
+  
+  Node (String val) {
+  this (val, null, null) n
+  }
+}
+  
+  
+
+    
