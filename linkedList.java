f0849fc36901ad942323e3dfc6e0d64b3328f394
jcheung29 <jcheung@albany.edu>
2016-10-15 13:29:56 -0400

    inserted a list
    added in a node list in alphabetical order with names of different
    fruits

--- linkedList.java
+++ linkedList.java
@@ -1,10 +1,17 @@
 import java.util.*;
 public class linkedList {
 
-class node {
+private class node {
   String value; //element 
   Node next; // reference to successor
   Node prev; // reference to predecessor 
+  
+  /**
+   constructor.
+   @param val the element to store in this node.
+   @param n the reference to the next node.
+   @param p the reference to the prev node.
+   */
   
   Node (String val, Node n, Node p) {
   value = val;
@@ -13,9 +20,44 @@ class node {
   }
   
   Node (String val) {
-  this (val, null, null) n
+  value = val;
+  next = null;
+  prev = null;
   }
 }
+
+//Referencing the first node in the list
+private Node first = null; 
+
+/**
+constructor
+build linked list
+*/
+
+public linkedList() {
+
+//incrementally build list
+//Alphabetical order of names 
+//Cherry Orange Peach Strawberry
+
+first = new Node ("Peach");
+first.next = new Node ("Strawberry");
+first.next.next = new Node ("Orange");
+first = new Node ("Cherry", first);
+
+String [] names = {"Apple", "Banana"};
+
+//use loop to add Apple and Banana to the front of the linked list 
+//to build the list
+//Apple Banana Cherry Orange Peach Strawberry
+
+for (String s: names)
+   first = new Node(s, first);
+   }
+
+
+
+
   
   
 
