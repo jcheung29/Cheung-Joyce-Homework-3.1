be68c01b146f36e985f6b0e6cfbaae9ad6167004
jcheung29 <jcheung@albany.edu>
2016-10-16 15:57:34 -0400

    Name Change
    Changed the name

---
+++ FruitList.java
@@ -0,0 +1,18 @@
+class FruitList {
+      private class Node {
+            private String word;
+            private int count = 0;
+            private linkedListList linkedList = 0;
+            private Node next = null;
+            }
+            
+            private Node head = new Node(" ", null); 
+            private Node tail = null;
+            
+            public void insert(String word, int linkedList) {
+               ++count;
+               misc.trace (count);
+               if (front = null) {
+                
+            }
+} 
