b4c1468f5960f45647d15f0d13d8c3213801f478
jcheung29 <jcheung@albany.edu>
2016-10-16 16:04:06 -0400

    Connect Node to String
    Tried to write a code that connects the Node to string. Still missing
    some things

---
+++ FruitList.java
@@ -0,0 +1,27 @@
+class FruitList {
+      private class Node {
+            int linenr;
+            Node link;
+            
+            Node (int fruit, Node newLink) {
+            linenr = fruit;
+            link = newLink; 
+            }
+            
+            private String word;
+            private int count = 0;
+            private linkedListList linkedList = 0;
+            private Node next = null;
+            }
+            
+            private Node head = new Node(" ", null); 
+            private Node tail = null;
+            
+            public void insert(String word, int linkedList) {
+               ++count;
+               misc.trace (count);
+               if (front = null) {
+                
+            }
+            }
+} 
