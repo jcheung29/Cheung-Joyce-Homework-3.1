a6e74987a58ebc7c186f74dc61f825727cdf4026
jcheung29 <jcheung@albany.edu>
2016-10-23 23:20:51 -0400

    deleted backwards class

--- Main.java
+++ Main.java
@@ -2,21 +2,14 @@ public class Main {
   
   public static void main(String [] arg) {
     linkedList L1List = new linkedList();
-    backwardsList BkwdList = new backwardsList();
     
       L1List.add("Apple");
       L1List.add("Banana");
       L1List.add("Cherry");
       L1List.add("Orange");
       L1List.add("Peach");
-      L1List.print();  
-      
-      BkwdList.add("Apple");
-      BkwdList.add("Banana");
-      BkwdList.add("Cherry");
-      BkwdList.add("Orange");
-      BkwdList.add("Peach");
-      BkwdList.print();
+      L1List.forward();  
+      L1List.backward();
       
   }
 }
--- linkedList.java
+++ linkedList.java
@@ -3,10 +3,12 @@ public class linkedList {
 
 //Referencing the first node in the list
 private Node first = null; 
+private Node last = null;
 
 public linkedList() {
 
 first = null;
+last = null;
 
 }
 
@@ -84,20 +86,142 @@ while (pointer.getNext() != null) {
             pointer.setNext(word);
             return;
             }
+      }  
+}
+}
+}
+}
+}
+
+public void sub(String node) { 
+Node pointer = new Node(node);
+
+  if (last == null)
+    last = pointer;
+    
+else 
+{
+Node word = first; 
+int compare = word.getData().compareTo(pointer.getData());
+
+if (compare == 0) {
+
+if (word.getNext() == null) {
+   word.setNext(word); 
+   pointer.setPrev(word);
+   }
+else {
+  pointer.setNext(word.getNext());
+  pointer.setPrev(word);
+  word.getNext().setPrev(pointer);
+  word.setNext(pointer); 
+}
+}
+
+
+if (compare > 0) {
+  pointer.setNext(word);
+  pointer.setPrev(pointer);
+  last = pointer; 
+}
+
+if (compare < 0) {
+  if (word.getNext() == null) {
+    word.setNext(pointer);
+    pointer.setPrev(word);
+}
+  else {
+    
+while (word.getNext() != null) {
+  word = word.getNext();
+  
+  if (compare == 0)
+{
+
+     if (word.getNext() == null) {
+         word.setNext(pointer); 
+         pointer.setPrev(word);
+         return;
+         }
+     
+     else {
+         pointer.setNext(word.getNext());
+         pointer.setPrev(word);
+         word.getNext().setPrev(pointer);
+         word.setNext(pointer); 
+         return;
+         }
+}
+
+     if (compare > 0) {
+        pointer.setNext(word);
+        pointer.setPrev(word.getPrev());
+        word.getPrev().setNext(pointer);
+        word.setPrev(pointer);
+        return;
+        }
+
+     if (compare < 0) {
+        if (word.getNext() == null) {
+            pointer.setPrev(word);
+            word.setNext(pointer);
+            return;
+            }
       }
 }
+  }
 }
 }
 }
-}
-public void print() {
+    public void delete(String node) {
+      Node pointer = first;
+      while (pointer != null) 
+
+      {
+        pointer = pointer.getNext();
+      }
+      
+      if (pointer.getNext == Node) {
+        Node prev = pointer.getPrev();
+        Node next = pointer.getNext();
+      }
+      
+      if (prev != null) { 
+        next.setPrev(prev);
+      }
+      else {
+        pointer = getNext;
+      }
+      
+      if (next != null) {
+        prev.setNext(next);
+      }
+      else {
+        prev.setNext(null);
+      }
+      
+
+    }
+
+public void forward() {
     Node pointer = first;
-    while (pointer!= null) {
+    while (pointer.getNext() != null) {
     System.out.print(pointer.getData() + " ");
     pointer = pointer.getNext();
     }
 }
+    public void backward() {
+    Node word = first;
+    while (word.getPrev() != null) {
+    System.out.print(word.getData() + " ");
+    word = word.getPrev(); 
+    }
 }
+}
+
+
+
+
 
 
 
